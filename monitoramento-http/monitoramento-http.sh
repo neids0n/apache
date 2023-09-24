738a75821f2ca17ca55b2044b1a9c0a779937ecd
Neidson de Oliveira <130571881+neids0n@users.noreply.github.com>
2023-09-24 20:10:23 -0300

    Update monitoramento-http.sh
    Atualização da descrição

--- monitoramento-http/monitoramento-http.sh
+++ monitoramento-http/monitoramento-http.sh
@@ -12,7 +12,7 @@ memoria=$(free -h | grep -i mem | awk '{ print $3 }')
 # Carga de CPU atual
 CPU=$(uptime | cut -d, -f4 |  awk '{ print $3 }')
 
-# O log será registrado. Caso o status HTTP for diferente de 200, o Apache será reiniciado registrando o ocorrido
+# O log será registrado em /var/log. Caso o status HTTP for diferente de 200, o Apache será reiniciado registrando o ocorrido
 if [ $status_http -eq 200 ]
 then
         echo "$(date +%d-%m-%Y,%H:%M:%S) -> Requisicoes = $requisicoes -> Uso de memória = $memoria -> Carga de CPU = $CPU -> O status HTTP é = $status_http" >> /var/log/monitoramento.log
